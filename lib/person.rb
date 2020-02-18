9de45572efbc8539a1414dfe2e6e10bcb2e5ab67
Abdul Khan <akhan17@outlook.com>
2020-02-18 18:06:17 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  
+  #getters
+  def name= name 
+    @name= name  
+  end 
+  
+  def job= job 
+    @job= job
+  end
+  
+  #setters
+  def name 
+    @name
+  end 
+  
+  def job 
+    @job
+  end
+  
+end 
